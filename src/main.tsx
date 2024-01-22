303b5f9b4bd38b2fcbe3085d7f4fbc7e07d42683
dsm23 <dsmurdoch357@gmail.com>
2024-01-22 12:20:46 +0000

    Add click-to-react-component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ClickToComponent } from "click-to-react-component";
+import App from "./App.tsx";
 
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
+    <ClickToComponent />
   </StrictMode>,
-)
+);
